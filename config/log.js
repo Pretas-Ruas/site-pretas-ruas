d508f93de7ab1a047f27f550f20eb44716b4709e
albertosouza <alberto.souza.99@gmail.com>
2018-03-03 03:47:10 -0300

    add support for log-local config

--- config/log.js
+++ config/log.js
@@ -1,21 +1,27 @@
-const winston = require('winston');
+try {
+  module.exports = require('./log-local.js');
+} catch(e) {
+  const winston = require('winston');
 
-const transports = [
-  new (winston.transports.Console)({
-    level: process.env.LOG_LV || 'info',
+  const transports = [
+    new (winston.transports.Console)({
+      level: process.env.LOG_LV || 'info',
+      json: true,
+      stringify: true
+    })
+  ];
+
+  module.exports.log = {
+    level: process.env.LOG_LV || 'info' ,
+    colorize: false,
+    timestamp: true,
     json: true,
-    stringify: true
-  })
-];
+    stringify: true,
+    prettyPrint: false,
+    depth: 5,
+    showLevel: true,
+    transports: transports
+  };
+}
 
-module.exports.log = {
-  level: process.env.LOG_LV || 'info' ,
-  colorize: false,
-  timestamp: true,
-  json: true,
-  stringify: true,
-  prettyPrint: false,
-  depth: 5,
-  showLevel: true,
-  transports: transports
-};
+
